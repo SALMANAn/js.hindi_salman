58fff156a31df91db925fcbe61cf4983a61e1289
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-15 04:43:56 +0000

    converstion of datatype

---
+++ 01_basic/converstion of datatype.js
@@ -0,0 +1,26 @@
+let scroe ="33ab"
+
+//console.log(typeof scroe);
+//console.log(typeof (scroe))
+
+let valueInNumber= Number(scroe);
+//console.log( valueInNumber)
+
+
+//"33"  => 33
+//  "33abc"   => NaN
+// true   =>1; false  => 0
+
+let isLoggedIn ="salman"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => falsae
+//"" =>False
+//"salman" => true
+
+
+let someNumber =35
+let stringNumber =String(someNumber)
+console.log(  stringNumber)
+console.log(typeof  stringNumber)
