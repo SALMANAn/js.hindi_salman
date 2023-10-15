0ea57f43dccee110e29510bf5dae7549ecad202f
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-15 03:19:22 +0000

    good progamming

---
+++ 01_basic/variable.js
@@ -0,0 +1,12 @@
+const accountId=466526
+let accountEmail="salmanstm@gamil.com"
+var accountPassword="Salomm@qns"
+accountCity="bihar"
+///accountId=266555  // not allowed
+console.log(accountId)
+
+accountEmail="salma@122"
+accountCity="panjab"
+accountPassword="salman@123"
+
+console.table([,accountEmail,accountCity,accountPassword])
