c97b6eab3f1ba8a216376e611e8402de2b218d06
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-16 16:50:22 +0000

    date and time best series

---
+++ 01_basic/date and time .js
@@ -0,0 +1,27 @@
+const score =400
+//console.log(score)
+//const balance=new Number(100)
+//console.log(balance)
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2))
+//const otherNumber =123.8966
+//console.log(otherNumber.toPrecision(4))
+//const hundreds=10000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//**************math************* */
+//console.log(Math)
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.6))
+//console.log(Math.ceil(4.2))
+//console.log(Math.floor(4.9));
+//console.log(Math.sqrt(4))
+//console.log(Math.min(2,3,5,7,8,9))
+//console.log(Math.max(2,3,5,7,8,9))
+console.log(Math.random())
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=10
+const max =20
+Math.random()*(max-min+1)+min
+console.log(Math.floor(Math.random()*(max-min+1))+min)
