573170ce307a11b81bf4efa9946cd2da5fa4419c
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-16 16:43:57 +0000

    string best part of javascript

---
+++ 01_basic/string.js
@@ -0,0 +1,33 @@
+let value =3
+let negValue =-value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+let str1="hello"
+let str2="salman"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log("2"+2+1);
+console.log(1+2+"2");
+console.log(true)
+console.log(+true)
+
+let num1 , num2 ,num3
+num1=num2=num3=2+2
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter)
+
+const name="salman"
+const repoCount=70
+console.log(name +repoCount+  " Value")
+//console.log("hello my name is${name.opper case} and my repo count is${repo}");
+//const gameName=new String("salman")
+console.log('Hello my name is $ {name}and my repo count is ${repoCount}');
+const gameName =new String("salmanic")
