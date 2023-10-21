ff3cd6fffab3a28afb6a54a5daa7c1c8fac5ca58
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-21 04:06:58 +0000

    555

--- 02_basic/array1.js
+++ 02_basic/array1.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+ console.log(myArr[1]);
 
 // Array methods
 
