b3803dd5670540dc1b87a24acd215e2d65682820
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-23 05:46:51 +0000

    Scope

--- 03_basic/02_Scope.js
+++ 03_basic/02_Scope.js
@@ -7,10 +7,56 @@ if (true) {
     let a=20
     const b=30
    // var c=45   //scope
-   console.log("INNER: " ,a);  // block scope
+   //
+   //console.log("INNER: " ,a);  // block scope
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+
+
+//nested function
+
+function one(){
+
+    const username="salman"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+   // console.log(website);
+
+    //two()
+
+}
+
+one()
+
+if (true) {
+    const username ="salman"
+    if (username=="salman") {
+        const website= "  youtube"
+        //console.log(username+website);
+
+        
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+// +++++++++++++intersting+++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
