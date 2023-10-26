8c7a291e1a7682d1a2ef59ac9caa82ec9e588bc1
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-26 17:32:48 +0000

    do while loop

---
+++ 05_loop/two.js
@@ -0,0 +1,23 @@
+let index=0
+while (index <= 10) {
+   // console.log(`Value of index ${index}`)
+    index=index+2
+       
+}
+
+let myArray=["salman","rehan","riya raj","farhat naaz"]
+let arr=0
+while (arr <myArray.length) {
+    //console.log(`value of is ${myArray[arr]}`);
+    arr=arr+1
+    
+}
+
+//do while
+
+let  score=11
+do {
+    console.log(`score is ${score}`);
+    score ++
+    
+} while (score <=10);
