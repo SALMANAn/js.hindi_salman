c21d0fc6c60b02c0918eef85f8048147c8d7a50a
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-28 05:18:23 +0000

    goood

---
+++ 05_loop/four.js
@@ -0,0 +1,17 @@
+const myObject={
+    js: "javascript",
+    cpp: "c++",
+    rb:"ruby",
+    swift:"swift by apple"
+}
+for(const key in myObject){
+//console.log(myObject[key]);
+//console.log(`${key} shortcut is for ${myObject[key]}`);
+
+}
+const progamming =["js","rb","py","java","cpp"]
+
+for (const key in progamming) {
+    console.log(progamming [key]);
+    
+}
--- 05_loop/three.js
+++ 05_loop/three.js
@@ -10,6 +10,33 @@ for (const num  of arr) {
 
 const greetings="Hello world"
 for (const greet of greetings) {
-    console.log(`Each char is ${(greet)}`);
+   // console.log(`Each char is ${(greet)}`);
     
 }
+
+//Maps
+const map=new Map()
+
+map.set('IN',"India")
+map.set('USA',"United states of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+//console.log(map)
+
+for (const [key, value] of map) {
+    
+    //console.log(key, ":-" value);
+
+}
+const myObject ={
+   // 'game1':'NFS',
+    //'game2':'spiderman',
+
+    //for (const (key,value) of myObject) {
+       // console.log(key,  ':-',value);
+        
+    //}
+
+}
+
