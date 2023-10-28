0eb4dad9d94b0dcf72150406092b17825d178087
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-10-28 06:11:40 +0000

    foreach loop

--- 05_loop/five.js
+++ 05_loop/five.js
@@ -1,1 +1,39 @@
+const coding=["salman","rehan","farhan","kalim","riya raj"]
+//coding.forEach( function  (item){
+   // console.log(item)
+//})
+ //coding.forEach((item)=>{
+   // console.log(item)
+//})
+
+//function printMe(item){
+   // console.log(item);
+//}
+//coding.forEach(printMe)
+
+//coding.forEach((item, index,arr)=>{
+//    console.log(item,index,arr);
+
+//})
+const myCoding=[
+   {
+      languageName:"Javascript",
+      languageFileName:"js"
+   },
+   {
+      languageName:"Python",
+      languageFileName:"py"
+   },
+   {
+      languageName:"java",
+      languageFileName:"java"
+   }
+
+]
+
+myCoding.forEach((item)=>{
+   console.log(item.languageName);
+})
+
+
 
