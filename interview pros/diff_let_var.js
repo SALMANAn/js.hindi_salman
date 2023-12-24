e5151a55bab52c64f76f85254f17fed6387f8c5b
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-12-24 05:06:03 +0000

    let var

---
+++ interview pros/diff_let_var.js
@@ -0,0 +1,18 @@
+function userDetails(username){
+    let age=24;
+        var salary=10000;
+
+    if(username){
+        console.log(salary);
+        console.log(age);
+        //let age=24;
+       // var salary=10000;
+
+    }
+    console.log(salary);
+    console.log(age);
+}
+userDetails()
+//let -> intorduce as part of es6,it is block scope,Hoisted but intialized
+
+// Var it has been availble from the begining of javascript ,It has function scope Variable will be Hoisted
