dba546d5c55dd0f85da9c5930835d94090d09a5c
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-12-24 04:48:50 +0000

    call apply bind

--- interview pros/call_bind_apply.js
+++ interview pros/call_bind_apply.js
@@ -1,4 +1,4 @@
-
+/*
 function greet(greeting,punctuation){
     console.log(greeting + " ," + this.name + punctuation);
  
@@ -61,7 +61,55 @@ function greet(greeting,punctuation){
  let newfun=printDeatails.bind(userDetails2,"Rajisthan","India")
  newfun()
  printDeatails.call(userDetails3,"Gujrat","India")   //call
- 
+*/
+/*
+ const employee1={firstName:"john",lastName:"Randoson"};
+ const employee2={firstName:"Kenwel",lastName:"Pollard"};
+ function invite(greeting1,greeting2){
+    console.log(
+       greeting1+ " "+ this.firstName+" "+ this.lastName+","+greeting2
+    );
+    console.log(
+        greeting2+ " "+ this.firstName+" "+ this.lastName+","+greeting1
+     );
+
+ }
+
+
+ invite.call(employee1,"Hello","How are you");
+ invite.call(employee2,"Hello","How are you");
+
+ invite.call(employee1,["Hello","who are you"]);
+
+*/
+var employee1 = { firstName: "John", lastName: "Rodson" };
+var employee2 = { firstName: "Jimmy", lastName: "Baily" };
+
+function invite(greeting1, greeting2) {
+  console.log(
+    greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2
+  );
+}
+
+invite.apply(employee1, ["Hello", "How are you?"]); // Hello John Rodson, How are you?
+invite.apply(employee2, ["Hello", "How are you?"]); // Hello Jimmy Baily, How are you?
+
+
+
+var employee1 = { firstName: "John", lastName: "Rodson" };
+var employee2 = { firstName: "Jimmy", lastName: "Baily" };
+
+function invite(greeting1, greeting2) {
+  console.log(
+    greeting1 + " " + this.firstName + " " + this.lastName + ", " + greeting2
+  );
+}
+
+var inviteEmployee1 = invite.bind(employee1);
+var inviteEmployee2 = invite.bind(employee2);
+
+inviteEmployee1("Hello", "How are you?"); // Hello John Rodson, How are you?
+inviteEmployee2("Hello", "How are you?"); // Hello Jimmy Baily, How are you?
  
  
  
