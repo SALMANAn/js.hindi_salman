177fbc303258c232ade1ca54906d6060f1aaa160
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-12-26 09:26:01 +0000

    fetch

--- interview pros/fetchapi.js
+++ interview pros/fetchapi.js
@@ -1,7 +1,39 @@
+/*
 let p=fetch("https://goweather.herouapp.com/weather/ny")
 p.then((value1)=>{
     
     return value1.json()
 }).then((value2)=>{
     console.log(value2);
+})
+*/
+let promise = new Promise(function (resolve, reject) {
+	const x = "geeksforgeeks";
+	const y = "geeksforgeeks"
+	if (x === y) {
+		resolve();
+	} else {
+		reject();
+	}
+});
+
+promise.
+	then(function () {
+		console.log('Success, You are a GEEK');
+	}).
+	catch(function () {
+		console.log('Some error has occurred');
+	});
+
+
+
+let promies=new Promise(function(resolve, reject){
+   // resolve("Salman is back")
+    reject("kuch bhi")
+})
+promies
+.then((SuccessMessage)=>{
+    console.log(SuccessMessage);
+}).catch((data)=>{
+    console.log("back to back",data);
 })
