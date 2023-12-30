bf203d8921fa6030650b3dc696802984dcc45430
Md Salman Ansari <78913412+SALMANAn@users.noreply.github.com>
2023-12-30 19:11:52 +0000

    fetch

--- FetchApi/threeApi/script.js
+++ FetchApi/threeApi/script.js
@@ -19,20 +19,58 @@ document.getElementById("btn").addEventListener("click",makerequest)
 
 //Promise Then |sigle data in  Browser
 
+// function makerequest(){
+//     console.log("Unmployee is big Issue");
+//     fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then((res)=>{
+//         if (!res.ok) {
+//             throw Error(res.statusText)
+//         }
+//         return res.json()
+//     }).then((data)=>{
+//         console.log(data);
+//         document.getElementById("pid").innerText=data.id
+//         document.getElementById("title").innerText=data.title
+//         document.getElementById("body").innerText=data.body
+//     }).catch((error)=>{
+//         console.log(error);
+//     })
+// }
+
+
+//Promise Then |    Multiple data  
+
 function makerequest(){
-    console.log("Unmployee is big Issue");
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    console.log("Thank you!");
+    fetch("https://jsonplaceholder.typicode.com/posts")
     .then((res)=>{
         if (!res.ok) {
-            throw Error(res.statusText)
+            throw error(res.statusText) 
         }
         return res.json()
+        console.log(res);
     }).then((data)=>{
         console.log(data);
-        document.getElementById("pid").innerText=data.id
-        document.getElementById("title").innerText=data.title
-        document.getElementById("body").innerText=data.body
+        data.forEach(element => {
+           console.log("Id:",element.id); 
+           console.log("Title:",element.title); 
+           console.log("body",element.body)
+        });
     }).catch((error)=>{
         console.log(error);
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
